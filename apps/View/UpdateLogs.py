eeac7dfa6ad020a6d4e1bef1aa2cddc779a4ebd2
Administrator <wenlong_zheng_za@mail.toyota.co.jp>
2017-06-07 10:19:00 +0900

    add base version for zkinderss

---
+++ apps/View/UpdateLogs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#A GAE web application to aggregate rss and send it to your kindle.
+#Visit https://github.com/cdhigh/KindleEar for the latest version
+#Contributors:
+# rexdf <https://github.com/rexdf>
+import web
+
+from apps.BaseHandler import BaseHandler
+from apps.dbModels import *
+from apps.utils import etagged
+
+class Uplogs(BaseHandler):
+    __url__ = "/updatelogs"
+    @etagged()
+    def GET(self, tips=None):
+        uplogs = UpdateLog.all()
+        return self.render('updatelogs.html', "Update log", current='updatelogs', uplogs=uplogs, tips=tips)
+    
+
+    def POST(self):
+#        name = '海贼王'.decode("utf")
+#        count = 867
+#        dl = UpdateLog(comicname=name, updatecount=count)
+#        dl.put()
+        uplogs = UpdateLog.all()
+        for log in uplogs:
+            name = log.comicname
+            count = int(web.input().get(name.encode("utf")))
+            print name.encode("utf") + ":" + str(count)
+            if count != log.updatecount:
+                log.delete()
+                dl = UpdateLog(comicname=name, updatecount=count)
+                dl.put()
+            elif count == 0:
+                log.delete()
+
+        newname = web.input().get("newname")
+        newcount = web.input().get("newcount")
+        if newname != "" and newcount != "":
+            dl = UpdateLog(comicname=newname, updatecount=int(newcount))
+            dl.put()
+            
+        tips = _("Settings Saved!")
+        
+        return self.GET(tips)
---
+++ books/sevensins.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import imghdr
+
+from bs4 import BeautifulSoup
+from base import BaseFeedBook, URLOpener
+
+def getBook():
+    return SevenSins
+
+class SevenSins(BaseFeedBook):
+    title               = u'七大罪'
+    description         = u'日本漫画家铃木央创作的少年漫画'
+    language            = 'zh-tw'
+    feed_encoding       = "big5"
+    page_encoding       = "big5"
+    mastheadfile        = "mh_comic.gif"
+    coverfile           = 'cv_sevensins.jpg'
+
+    def Items(self, opts=None, user=None):
+        """
+        生成器，返回一个图片元组，mime,url,filename,content,brief,thumbnail
+        """
+        urls = self.ParseFeedUrls()
+        opener = URLOpener(self.host, timeout=self.timeout, headers=self.extra_header)
+        for section, ftitle, url, desc in urls:
+            opener = URLOpener(self.host, timeout=self.timeout, headers=self.extra_header)
+            result = opener.open(url)
+            article = result.content 
+            if not article:
+                continue
+           
+            imgtype = imghdr.what(None, article)
+            imgmime = r"image/" + imgtype
+            fnimg = "img%d.%s" % (self.imgindex, 'jpg' if imgtype=='jpeg' else imgtype)
+            yield (imgmime, url, fnimg, article, None, None)
+           
+            tmphtml = '<html><head><title>Picture</title></head><body><img src="%s" /></body></html>' % fnimg
+            yield (section, url, ftitle, tmphtml, '', None)
+
+    def ParseFeedUrls(self):
+        mainurl = "http://www.cartoonmad.com"
+        urls = []
+        opener = URLOpener(self.host, timeout=60)
+        result = opener.open(mainurl)
+        if result.status_code != 200:
+            self.log.warn('fetch rss failed:%s' % mainurl)
+            return []
+        content = result.content.decode(self.feed_encoding)
+
+        comic_name = '七原罪'.decode("utf8")
+        title = '七大罪'.decode("utf8")
+        soup = BeautifulSoup(content, "lxml")
+        mhnew = soup.findAll("div", {"style": 'overflow:hidden;'})
+        
+        for obj in mhnew:
+            name = obj.find("a").text
+            if ( name[0:len(comic_name)] == comic_name ):
+                href = "http://www.cartoonmad.com" + obj.find("a").get("href")
+                print href
+                comic_opener = URLOpener(self.host, timeout=60)
+                comic_page = comic_opener.open(href)
+                if comic_page.status_code != 200:
+                    self.log.warn('fetch rss failed:%s' % mainurl)
+                    return []
+
+                comic_content = comic_page.content.decode(self.feed_encoding)
+                comic_body = BeautifulSoup(comic_content, "lxml")
+                ul = comic_body.find("select").findAll("option")
+                for mh in ul:
+                    mhhref = mh.get("value")
+                    if mhhref:
+                        pagehref = "http://www.cartoonmad.com/comic/" + mhhref
+                        pageopener = URLOpener(self.host, timeout=60)
+                        pageresult = pageopener.open(pagehref)
+                        if pageresult.status_code != 200:
+                            self.log.warn('fetch rss failed:%s' % mainurl)
+                            return []
+                        body = pageresult.content.decode(self.feed_encoding)
+                        sp = BeautifulSoup(body, "lxml")
+                        mhpic = sp.find("img", {"oncontextmenu": 'return false'}).get("src")
+                        print mhpic
+                        urls.append( (title, mh.text, mhpic, None))
+        return urls
