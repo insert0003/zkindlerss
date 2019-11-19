567c998dc96d11f55664e09fe96501579c115b9e
insert0003 <zheng00032003@gmail.com>
2019-11-19 14:21:10 +0900

    修改pufei和manhuagui

--- books/comic/manhuaguibase.py
+++ books/comic/manhuaguibase.py
@@ -33,30 +33,50 @@ class ManHuaGuiBaseBook(BaseComicBook):
     #获取图片信息
     def get_node_online(self, input_str):
         opts_str = 'console.log(%s)' % input_str.encode("utf-8")
-        try:
-            self.log.warn("Try use runoob execution nodejs.")
-            url = "https://m.runoob.com/api/compile.php"
-            params = {"code":opts_str, "stdin":"", "language":"4", "fileext":"node.js"}
-            params = urllib.urlencode(params)
-            req = urllib2.Request(url)
-            req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-            req.add_data(params)
+        for njRetry in range(3):
+            try:
+                if njRetry == 0:
+                    self.log.info("Try use rextester execution nodejs.")
+                    url = "https://rextester.com/rundotnet/Run"
 
-            res = urllib2.urlopen(req)
-            result = json.loads(res.read())
-            return result["output"]
-        except:
-            self.log.warn("Try use tutorialspoint execution nodejs.")
-            url = "https://tpcg.tutorialspoint.com/tpcg.php"
-            params = {"lang":"node", "device":"", "code":opts_str, "stdin":"", "ext":"js", "compile":0, "execute": "node main.js", "mainfile": "main.js", "uid": 4203253 }
-            params = urllib.urlencode(params)
-            req = urllib2.Request(url)
-            req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-            req.add_data(params)
+                    params = {"LanguageChoiceWrapper":"23", "EditorChoiceWrapper":1, "LayoutChoiceWrapper":1, "Program":opts_str, "Input":"", "Privacy":"", "PrivacyUsers":"", "Title": "", "SavedOutput": "", "WholeError": "", "WholeWarning": "", "StatsToSave": "", "CodeGuid": "", "IsInEditMode": False, "IsLive": False }
+                    params = urllib.urlencode(params)
+                    req = urllib2.Request(url)
+                    req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+                    req.add_data(params)
 
-            res = urllib2.urlopen(req)
-            result = BeautifulSoup(res.read(), 'html.parser')
-            return result.find("br").text
+                    res = urllib2.urlopen(req)
+                    result = json.loads(res.read())
+                    return result["Result"]
+                elif njRetry == 1:
+                    self.log.info("Try use tutorialspoint execution nodejs.")
+                    url = "https://tpcg.tutorialspoint.com/tpcg.php"
+                    params = {"lang":"node", "device":"", "code":opts_str, "stdinput":"", "ext":"js", "compile":0, "execute": "node main.js", "mainfile": "main.js", "uid": 9920007 }
+                    params = urllib.urlencode(params)
+                    req = urllib2.Request(url)
+                    req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+                    req.add_data(params)
+
+                    res = urllib2.urlopen(req)
+                    result = BeautifulSoup(res.read(), 'html.parser')
+                    return result.find("br").text
+                else:
+                    self.log.info("Try use runoob execution nodejs.")
+                    url = "https://tool.runoob.com/compile.php"
+                    params = {"code":opts_str, "token":"4381fe197827ec87cbac9552f14ec62a", "language":"4", "fileext":"node.js"}
+                    params = urllib.urlencode(params)
+                    req = urllib2.Request(url)
+                    req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+                    req.add_data(params)
+
+                    res = urllib2.urlopen(req)
+                    result = res.read()
+                    result = result.replace("\/", "/")
+                    result = json.loads(result)
+                    return result["output"]
+            except Exception, e:
+                self.log.warn('str(Exception):{}'.format(str(e)))
+                continue
 
     #获取漫画章节列表
     def getChapterList(self, url):
@@ -92,12 +112,15 @@ class ManHuaGuiBaseBook(BaseComicBook):
             self.log.warn('chapterList href is not exist.')
             return chapterList
 
-        for aindex in range(len(lias)):
-            rindex = len(lias)-1-aindex
-            href = "https://m.manhuagui.com" + lias[rindex].get("href")
-            chapterList.append((unicode(lias[rindex].string), href))
+        for index, a in enumerate(lias):
+            href = self.urljoin("https://m.manhuagui.com", a.get('href', ''))
+            bstr = a.find("b")
+            if bstr == None:
+                chapterList.append((u'第%d话'%(index+1), href))
+            else:
+                chapterList.append((unicode(bstr.contents[0]), href))
 
-        return chapterList
+        return list(reversed(chapterList))
 
     #获取漫画图片列表
     def getImgList(self, url):
--- books/comic/pufeibase.py
+++ books/comic/pufeibase.py
@@ -8,7 +8,6 @@ from lib.autodecoder import AutoDecoder
 from books.base import BaseComicBook
 from bs4 import BeautifulSoup
 import urllib, urllib2, imghdr
-from base64 import b64decode, b64encode
 
 class PuFeiBaseBook(BaseComicBook):
     accept_domains = ("http://www.pufei.net", "http://m.pufei.net")
@@ -44,38 +43,58 @@ class PuFeiBaseBook(BaseComicBook):
 
         for index, a in enumerate(lias):
             href = self.urljoin("http://www.pufei.net", a.get('href', ''))
-            span = a.find("span")
-            chapterList.append((unicode(lias[len(lias)-1-index].string), href))
+            chapterList.append((unicode(lias[index].string), href))
 
-        return chapterList
+        return list(reversed(chapterList))
 
     #获取图片信息
     def get_node_online(self, input_str):
         opts_str = 'console.log(%s)' % input_str.encode("utf-8")
-        try:
-            self.log.info("Try use tutorialspoint execution nodejs.")
-            url = "https://tpcg.tutorialspoint.com/tpcg.php"
-            params = {"lang":"node", "device":"", "code":opts_str, "stdin":"", "ext":"js", "compile":0, "execute": "node main.js", "mainfile": "main.js", "uid": 4203253 }
-            params = urllib.urlencode(params)
-            req = urllib2.Request(url)
-            req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-            req.add_data(params)
 
-            res = urllib2.urlopen(req)
-            result = BeautifulSoup(res.read(), 'html.parser')
-            return result.find("br").text
-        except:
-            self.log.info("Try use runoob execution nodejs.")
-            url = "https://m.runoob.com/api/compile.php"
-            params = {"code":opts_str, "stdin":"", "language":"4", "fileext":"node.js"}
-            params = urllib.urlencode(params)
-            req = urllib2.Request(url)
-            req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-            req.add_data(params)
+        for njRetry in range(3):
+            try:
+                if njRetry == 0:
+                    self.log.info("Try use rextester execution nodejs.")
+                    url = "https://rextester.com/rundotnet/Run"
 
-            res = urllib2.urlopen(req)
-            result = json.loads(res.read())
-            return result["output"]
+                    params = {"LanguageChoiceWrapper":"23", "EditorChoiceWrapper":1, "LayoutChoiceWrapper":1, "Program":opts_str, "Input":"", "Privacy":"", "PrivacyUsers":"", "Title": "", "SavedOutput": "", "WholeError": "", "WholeWarning": "", "StatsToSave": "", "CodeGuid": "", "IsInEditMode": False, "IsLive": False }
+                    params = urllib.urlencode(params)
+                    req = urllib2.Request(url)
+                    req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+                    req.add_data(params)
+
+                    res = urllib2.urlopen(req)
+                    result = json.loads(res.read())
+                    return result["Result"]
+                elif njRetry == 1:
+                    self.log.info("Try use tutorialspoint execution nodejs.")
+                    url = "https://tpcg.tutorialspoint.com/tpcg.php"
+                    params = {"lang":"node", "device":"", "code":opts_str, "stdinput":"", "ext":"js", "compile":0, "execute": "node main.js", "mainfile": "main.js", "uid": 9920007 }
+                    params = urllib.urlencode(params)
+                    req = urllib2.Request(url)
+                    req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+                    req.add_data(params)
+
+                    res = urllib2.urlopen(req)
+                    result = BeautifulSoup(res.read(), 'html.parser')
+                    return result.find("br").text
+                else:
+                    self.log.info("Try use runoob execution nodejs.")
+                    url = "https://tool.runoob.com/compile.php"
+                    params = {"code":opts_str, "token":"4381fe197827ec87cbac9552f14ec62a", "language":"4", "fileext":"node.js"}
+                    params = urllib.urlencode(params)
+                    req = urllib2.Request(url)
+                    req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+                    req.add_data(params)
+
+                    res = urllib2.urlopen(req)
+                    result = res.read()
+                    result = result.replace("\/", "/")
+                    result = json.loads(result)
+                    return result["output"]
+            except Exception, e:
+                self.log.warn('str(Exception):{}'.format(str(e)))
+                continue
 
     #获取漫画图片列表
     def getImgList(self, url):
